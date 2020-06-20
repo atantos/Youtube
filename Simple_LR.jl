8f949442a85b637e4b3380b934709cedbb9256fb
ghurley <ghurley@users.noreply.github.com>
2020-06-19 18:50:36 -0700

    Remove comma handling code
    In favor of the built-in functionality in the CSV package.

--- Simple_LR.jl
+++ Simple_LR.jl
@@ -9,26 +9,11 @@ using DataFrames
 using CSV
 
 # Reading a csv file from github into a data frame stored to the variable GLC_test4
-GLC_test4 = CSV.read(download("https://raw.githubusercontent.com/atantos/Youtube/master/GLC_test4.csv"))
+GLC_test4 = CSV.read(download("https://raw.githubusercontent.com/atantos/Youtube/master/GLC_test4.csv"),
+                     decimal=',')
 
 # Reading a local csv file
 GLC_test4 = CSV.read("<path_to_file>/GLC_test4.csv")
-
-# Creating a function for converting commas into dots
-function comma_rep(x)
-    replace(x, r"," => ".")
-end
-
-# Creating a function for converting strings into floats
-function convert_str_float64(x)
-    parse(Float64, x)
-end
-
-# Applying the previously stored and loaded functions on the cells of GLC_test4
-for x in Symbol.(names(GLC_test4))
-    GLC_test4[!,x] = map(comma_rep, GLC_test4[!,x])
-    GLC_test4[!,x] = map(convert_str_float64, GLC_test4[!,x])
-end
 
 # Fitting a linear regression model and storing the resulting model object into GLC_ols
 GLC_ols = lm(@formula(GLC_partI ~ GLC_partII), GLC_test4) # R-style notation
